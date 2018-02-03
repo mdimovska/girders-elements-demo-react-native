b69af1c2d4bea4df56a7d4d3f845039fd8eef998
Milena Dimovska <Milena.Dimovska@netcetera.com>
2018-02-03 14:57:06 +0100

    Remove unused import

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppRegistry, Platform } from 'react-native'
+import { Platform } from 'react-native'
 
 import { Engine } from '@girders-elements/core'
 import './src/elements' // register all elements in Girders Elements
