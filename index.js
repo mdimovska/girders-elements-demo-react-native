37866b2fa258ccbca6dbfdc0bfc3fee89eb304aa
Milena Dimovska <Milena.Dimovska@netcetera.com>
2018-02-03 14:59:41 +0100

    Some clean up. Add core-js dependency and include the needed pollyfill.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import 'core-js/modules/es6.object.set-prototype-of'
+import { AppRegistry } from 'react-native'
+import App from './App'
 
-AppRegistry.registerComponent('nativeApp', () => App);
+AppRegistry.registerComponent('nativeApp', () => App)
