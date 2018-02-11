50b78bdd8a3cedeb1a5e63aa3bd3ce63d9d3643d
Milena Dimovska <Milena.Dimovska@netcetera.com>
2018-02-11 13:18:07 +0100

    Move polyfills to separate files

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Platform } from 'react-native'
+import './polyfills'
 
 import { Engine } from '@girders-elements/core'
 import './src/elements' // register all elements in Girders Elements
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import 'core-js/modules/es6.object.set-prototype-of'
 import { AppRegistry } from 'react-native'
 import App from './App'
 
