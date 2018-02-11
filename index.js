983d7ad217bce72d93065b391e1d5cdf7b7266aa
Milena Dimovska <Milena.Dimovska@netcetera.com>
2018-02-11 13:46:21 +0100

    Move code from App.js to index.js and remove App.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import { AppRegistry } from 'react-native'
-import App from './App'
+import React from 'react'
+import { AppRegistry, Platform } from 'react-native'
+import './polyfills'
 
+import { Engine } from '@girders-elements/core'
+import './src/elements' // register all elements in Girders Elements
+
+import initialState from './config/init'
+import config from './config'
+
+config.init([Platform.OS, 'native'])
+
+const App = () => <Engine config={config} initState={initialState} />
 AppRegistry.registerComponent('nativeApp', () => App)
